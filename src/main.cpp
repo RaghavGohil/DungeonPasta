db33a8d80ef99e669962a04379cb5a02d2de62ce
Raghav Gohil <raghav15102004@gmail.com>
2023-02-20 19:33:18 +0530

    SDL added

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<string.h>
+#include "../inc/SDL2/include/SDL.h"
 
-int main()
+int main(int argc,char *argv[])
 {
     std::cout << "Hello World!";
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window = SDL_CreateWindow("title",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,600,400,SDL_WINDOW_SHOWN);
     return 0;
 }
