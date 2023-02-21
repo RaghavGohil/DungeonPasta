9a375057b2097fd0ba4460b2d75fe6800140ac1f
Raghav Gohil <raghav15102004@gmail.com>
2023-02-21 17:27:22 +0530

    Working sdl window generation succesful. Use branching from now to release updates!

--- build.bat
+++ build.bat
@@ -1,10 +1,6 @@
 @echo off
 echo compiling using cl.exe -- msvc 
-@REM echo -lstdc++ options links the standard libraries.
-@REM echo -L stands for library search path.
-@REM echo -I searches and includes all the header files.
-@REM echo -Wall gives all the warnings.
-@REM g++ ../src/main.cpp -lstdc++ -Wall -lSDL2main -lSDL2 -I ../inc/SDL
+@REM note: sdl2.dll is added to the path env so please add it manually.
 pushd build
 @REM vcvars32.bat // use this once..
 cl /Fea.exe ../src/main.cpp /Wall /EHsc /I "../inc/SDL" /link ../lib/SDL2main.lib ../lib/SDL2.lib
--- src/main.cpp
+++ src/main.cpp
@@ -7,29 +7,66 @@
 #define SCREEN_HEIGHT 480
 #define TITLE "game"
 
+bool init();
+bool loadMedia();
+void close();
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hpInstance, LPSTR nCmdLine, int iCmdShow)
 {   
 
-    //create window and surface.
+    //Create window and surface
     SDL_Window *window = NULL;
     SDL_Surface *screenSurface = NULL;
 
-    //init sdl(without initializing you cannot call the sdl functions).
+    //Init sdl(without initializing you cannot call the sdl functions)
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
-        std::cout << "SDL could not be initialized. " ;//<< SDL_GetError() << std::endl;
+    
+        std::cout << "SDL could not be initialized. " << SDL_GetError() << std::endl;
+    
     }
 
     else
     {
+    
         //Create window
         window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
         
         if( window == NULL )
         {
+    
             std::cout << "Window could not be created. " << SDL_GetError() << std::endl;
+    
         }
+
+        else
+        {
+    
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //SDL_Delay(ms); can be used to create a delay of ms.
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+    
+        }
+
+        //Destroy window and free from the memory.
+        SDL_DestroyWindow( window );
+
+        //Quit SDL subsystems
+        SDL_Quit();
+    
     }
 
     return 0;
+
 }
+
